9c3312a81b71e4052adad9a30cc7413802af349a
GsRawat <gsrawat0512@gmail.com>
2020-04-15 20:02:59 +0530

    compiled and created the bin package directory

--- src/com/gsrawat/filemanager/Main.java
+++ src/com/gsrawat/filemanager/Main.java
@@ -2,6 +2,6 @@ package com.gsrawat.filemanager;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.pirntln("Hello to My Project.");
+        System.out.println("Hello to My Project.");
     }
 }
