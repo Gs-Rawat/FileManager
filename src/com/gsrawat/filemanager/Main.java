ca1ed525cc97f76346f91d0129774dee3456c703
GsRawat <gsrawat0512@gmail.com>
2020-04-16 11:31:21 +0530

    Added the switch case for executing different commands

--- src/com/gsrawat/filemanager/Main.java
+++ src/com/gsrawat/filemanager/Main.java
@@ -2,6 +2,31 @@ package com.gsrawat.filemanager;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello to My Project.");
+        String choice = args[0];
+        switch (choice) {
+            case "read":
+                Reader.read();  
+                break;
+            
+            case "write":
+                Writer.write();
+                break;
+
+            case "mkdir":
+                Directory.mkdir(); 
+                break;
+            
+            case "cp":
+                Copy.cp();
+                break;
+            
+            case "del":
+                Delete.del();
+                break;
+        
+            default:
+                break;
+        }
+
     }
 }
